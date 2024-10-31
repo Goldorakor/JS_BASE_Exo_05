57289b59b883a68d63118715f3ac0542fd2262d0
Michaël HEID <michael123456222444666@gmail.com>
2024-10-31 08:51:21 +0100

    Add files via upload

---
+++ js/quotes_test.js
@@ -0,0 +1,47 @@
+var quotes = {
+    "Serge Karamazov" : "Barrez vous **** de mimes.",
+    "Georges Abitbol" : "Le train de tes injures roule sur le rail de mon indifférence.",
+    "Hubert Bonisseur de la Bath" : "Ca ne prenait pas beaucoup plus de temps.",
+    "Moulinier" : "L'habit ne fait pas le moine mais il fait l'agent ... même non titularisé.",
+    "Orson Welles" : "C'est du vol et du plagiat. J'aime pas trop les voleurs et ..."
+};
+
+
+/*
+En JavaScript, un tableau associatif est souvent représenté par un objet, car JavaScript ne supporte pas directement les tableaux associatifs comme certains autres langages. Voici comment tu peux instancier un tableau associatif :
+
+
+
+1. Utilisation d'un objet :
+
+const tableauAssociatif = {
+    cle1: "valeur1",
+    cle2: "valeur2",
+    cle3: "valeur3"
+};
+
+// Accéder à une valeur
+console.log(tableauAssociatif.cle1); // Affiche "valeur1"
+
+
+
+
+
+2. Utilisation de la notation par crochets :
+
+const tableauAssociatif = {};
+
+// Ajouter des paires clé-valeur
+tableauAssociatif['cle1'] = "valeur1";
+tableauAssociatif['cle2'] = "valeur2";
+
+// Accéder à une valeur
+console.log(tableauAssociatif['cle2']); // Affiche "valeur2"
+
+
+
+Dans les deux cas, tu peux ajouter, modifier ou accéder aux valeurs en utilisant les clés associées. Les objets sont la méthode recommandée pour créer des structures de données similaires à des tableaux associatifs en JavaScript.
+
+
+
+*/
